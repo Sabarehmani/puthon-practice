1f4f31b51ed2d4af2c51b4e8516b069434d6c353
Sabarehmani <sabarehmani287@gmail.com>
2024-08-17 08:40:37 -0700

    added two files

---
+++ test.py
@@ -0,0 +1,64 @@
+# now in this class i will teach you  how to tale input from user 
+
+# i will share this with you ok
+# thisis an function for input from user ok?
+
+#print("ENTER ANY NUMBER = ") 
+#input()
+
+# will asked input from user ok 
+
+# now for input we dont need print function 
+
+#input("ENTER any number = ")
+
+# have you see diiifernce? 
+
+# now we can store input in variable
+
+
+#num1 = input("ENTER any number = ")
+
+#now this is very import 
+
+
+# the thing we get from user is  string at every time 
+
+# see this  consider above example 
+
+#print("this is you number that you enter ", num1 )
+
+#print(type(num1))
+
+#<class 'str'> str mean string 
+#-------------------------------------------------------------------
+# see here this is string concatenation (spell mistake maybe )
+
+#num1 = input("ENTER ANY number")
+#a = 12 
+
+#print(f"this is sum of two number {num1} and {a} ") #dont see how i write it i will tell you in next module jut see concepts ok
+#print(num1+a)
+# now see what he tell me 
+
+
+# now list to to me these are diiferent functios
+
+#int() #----->> convert everything in to integers
+
+#String()   #------>> convert evetyhing into stirng 
+
+# and mnay other
+
+
+# now lets fixed above code
+#---------------------------------------------------------------------
+
+num1 = input("ENTER ANY number")
+num1 = int(num1 )# now it will convert sting into integer 
+a = 12 
+
+print(f"this is sum of two number {num1} and {a} ") #dont see how i write it i will tell you in next module jut see concepts ok
+print(num1+a)
+
+
